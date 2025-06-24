539bbefd22b048255d1baa89820917862b6ae245
Federico Baldi <133745279+fedeCode00@users.noreply.github.com>
2025-06-24 12:04:55 +0200

    Update train_5_focal_weighted.py

--- train_5_focal_weighted.py
+++ train_5_focal_weighted.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader
 from torchvision import transforms
 import time
 from PIL import Image
@@ -11,64 +11,28 @@ import numpy as np
 from datasets.gta5 import GTA5
 from datasets.cityscapes import CityscapesTarget
 from models.bisenet.build_bisenet import BiSeNet
-from utils import compute_mIoU, fast_hist, per_class_iou, poly_lr_scheduler
+from utils import fast_hist, per_class_iou, poly_lr_scheduler
 import math
 import random
 from datasets.transforms import JointTransform
 from torch.utils.data import Subset
 from torch.cuda import amp
-from train_gta5 import FCDiscriminator
+from models.discriminator import FCDiscriminator
 from itertools import cycle
 import torch.nn.functional as F
+from losses import FocalLoss
 
 
-#################### FocalLoss personalizzata ####################
-class FocalLoss(nn.Module):
-    def __init__(self, class_weights=None, ignore_index=255, gamma=1.0):
-        super(FocalLoss, self).__init__()
-        self.ignore_index = ignore_index
-        self.gamma = gamma
-        if class_weights is not None:
-            self.class_weights = class_weights.view(1, -1)
-        else:
-            self.class_weights = None
-
-    def forward(self, input, target):
-        # input: [N, C, H, W]
-        # target: [N, H, W]
-        input, target = self.flatten(input, target, self.ignore_index)
-        log_probs = torch.log_softmax(input, dim=1)
-        probs = torch.exp(log_probs)               
-
-        focal_factor = (1 - probs) ** self.gamma     
-
-        target_log_probs = log_probs.gather(1, target.unsqueeze(1)).squeeze(1)     
-        target_focal_factor = focal_factor.gather(1, target.unsqueeze(1)).squeeze(1)  
-
-        ce_loss = -target_log_probs  
-
-        if self.class_weights is not None:
-            weights = self.class_weights[0].gather(0, target)
-            loss = weights * target_focal_factor * ce_loss
-        else:
-            loss = target_focal_factor * ce_loss
-
-        return loss.mean()
-
-    def flatten(self, input, target, ignore_index):
-        num_classes = input.size(1)
-        input = input.permute(0, 2, 3, 1).contiguous().view(-1, num_classes)
-        target = target.view(-1)
-        mask = target != ignore_index
-        return input[mask], target[mask]
-
+#################### CONFIGURAZIONE ####################
+CONTEXT_PATH = 'resnet18'
+ALPHA = 1
 NUM_CLASSES = 19
-BATCH_SIZE = 8
+BATCH_SIZE = 4
 EPOCHS = 50
 LEARNING_RATE = 2.5e-2
 IMG_SIZE = (720, 1280)
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-ALPHA = 1
+
 CLASS_NAMES = [
     'road', 'sidewalk', 'building', 'wall', 'fence', 'pole',
     'traffic light', 'traffic sign', 'vegetation', 'terrain',
@@ -76,6 +40,8 @@ CLASS_NAMES = [
     'train', 'motorcycle', 'bicycle'
 ]
 
+
+#################### TRANSFORM ####################
 input_transform = transforms.Compose([
     transforms.Resize(IMG_SIZE),
     transforms.ToTensor(),
@@ -86,9 +52,17 @@ target_transform = transforms.Compose([
     transforms.Resize(IMG_SIZE, interpolation=Image.NEAREST),
 ])
 
+transform_cityscapes = transforms.Compose([
+    transforms.Resize((512, 1024)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+
+#################### DATASET ####################
 dataset_root = "/kaggle/working/punto-3/Seg_sem_25/Seg_sem_25/datasets/GTA5/GTA5"
 
-train_joint_transform = JointTransform(input_transform, target_transform, augment=True, strategy='jitter')
+train_joint_transform = JointTransform(input_transform, target_transform, augment=True, strategy='flip-jitter')
 val_joint_transform = JointTransform(input_transform, target_transform, augment=False, strategy='none')
 
 train_full = GTA5(root=dataset_root, joint_transform=train_joint_transform)
@@ -106,13 +80,6 @@ val_indices = indices[train_size:]
 train_dataset = Subset(train_full, train_indices)
 val_dataset = Subset(val_full, val_indices)
 
-# TRANSFORM
-transform_cityscapes = transforms.Compose([
-    transforms.Resize((512, 1024)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
-
 target_dataset = CityscapesTarget(
     root='/kaggle/working/punto-3/Seg_sem_25/Seg_sem_25/datasets/Cityscapes/Cityscapes/Cityspaces',
     split='train',
@@ -123,6 +90,8 @@ train_loader_gta = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True
 val_loader_gta = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4)
 cityscapes_loader = DataLoader(target_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
 
+
+#################### COMPUTE WEIGHTS ####################
 def compute_pixel_frequency(dataloader, num_classes):
     class_pixel_count = np.zeros(num_classes, dtype=np.int64)
     num_images = len(dataloader.dataset)
@@ -167,21 +136,25 @@ print("Class Weights (Median Frequency Balancing):")
 print(weights)
 
 normalized_weights = weights / weights.sum() * len(weights)
+print(normalized_weights)
 weights_tensor = torch.tensor(normalized_weights, dtype=torch.float32).to(DEVICE)
+
 
 #################### MODEL & DISCRIMINATOR ####################
 model = BiSeNet(num_classes=NUM_CLASSES, context_path='resnet18').to(DEVICE)
 D = FCDiscriminator(num_classes=NUM_CLASSES).to(DEVICE)
 
 if torch.cuda.device_count() > 1:
-    print(f"🚀 Usando {torch.cuda.device_count()} GPU!")
+    print(f"Usando {torch.cuda.device_count()} GPU!")
     model = nn.DataParallel(model)
     D = nn.DataParallel(D)
 
 model = model.to(DEVICE)
 D = D.to(DEVICE)
 
-criterion = FocalLoss(class_weights=weights_tensor, ignore_index=255, gamma=1.0)
+
+#################### LOSS & OPTIMIZER ####################
+criterion = FocalLoss(ignore_index=255, gamma=1.0)
 optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=0.9, weight_decay=1e-4)
 optimizer_D = optim.Adam(D.parameters(), lr=1e-4, betas=(0.9, 0.99))
 scaler = GradScaler()
@@ -192,6 +165,7 @@ max_iter = steps_per_epoch * EPOCHS
 
 bce_loss = nn.BCEWithLogitsLoss()
 lambda_adv = 0.001
+
 
 #################### PAPER LOSS ####################
 def SegLoss(output, target, criterion, cx1=None, cx2=None, alpha=1.0):
@@ -236,6 +210,7 @@ def train(model, train_loader, optimizer, criterion, device, num_classes, epoch)
                 src_cx1, src_cx2 = source_outputs[1], source_outputs[2]
                 source_outputs = source_outputs[0]
 
+
             seg_loss = SegLoss(source_outputs, source_targets, criterion, src_cx1, src_cx2, alpha=ALPHA)
             target_outputs = model(target_inputs)
             if isinstance(target_outputs, tuple): target_outputs = target_outputs[0]
@@ -244,6 +219,9 @@ def train(model, train_loader, optimizer, criterion, device, num_classes, epoch)
             adv_loss = lambda_adv * bce_loss(D_out, torch.full(D_out.size(), 0.0, device=device))
 
             total_loss = seg_loss + adv_loss
+            if torch.isnan(total_loss) or torch.isinf(total_loss):
+                print("❌ Attenzione: total_loss contiene NaN o Inf! Interrompo il training.")
+                exit(1)
 
         scaler.scale(total_loss).backward()
         scaler.step(optimizer)
@@ -282,8 +260,7 @@ def train(model, train_loader, optimizer, criterion, device, num_classes, epoch)
     current_lr = optimizer.param_groups[0]['lr']
 
     print(f"[Epoch {epoch}] | Total Loss: {avg_total_loss:.4f} | Seg Loss: {avg_seg_loss:.4f} | Adv Loss: {avg_adv_loss:.4f} | Pixel Acc: {pixel_acc:.4f} | Time: {epoch_time:.1f}s | LR: {current_lr:.6f}")
-    
-  return avg_total_loss, pixel_acc
+    return avg_total_loss, pixel_acc
 
 
 #################### VALIDATION ####################
@@ -292,7 +269,6 @@ def validate(model, val_loader, criterion, device, num_classes, epoch):
     val_loss = total_correct = total_pixels = 0
     total_batches = len(val_loader)
     start_time = time.time()
-  
     hist = np.zeros((num_classes, num_classes))
 
     with torch.no_grad():
@@ -331,6 +307,7 @@ def validate(model, val_loader, criterion, device, num_classes, epoch):
         print(f"  {CLASS_NAMES[idx]}: {iou_cls:.2f}%")
 
     return pixel_acc, mIoU
+    
 
 #################### MAIN ####################
 if __name__ == '__main__':
@@ -341,7 +318,7 @@ if __name__ == '__main__':
         print(f"Epoch {epoch}/{EPOCHS}")
         _, _ = train(model, train_loader_gta, optimizer, criterion, DEVICE, NUM_CLASSES, epoch)
         pixel_acc, mean_iou = validate(model, val_loader_gta, criterion, DEVICE, NUM_CLASSES, epoch)
-        
+
         torch.cuda.empty_cache()
 
         if mean_iou > best_miou:
