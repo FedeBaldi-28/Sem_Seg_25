12f770fa90b9b118987124c636ce7a09a3899b3d
Ilaria Parodi <146097375+ilariaparodi@users.noreply.github.com>
2025-06-25 17:55:23 +0200

    Update train_3b_flip.py

--- train_3b_flip.py
+++ train_3b_flip.py
@@ -18,7 +18,7 @@ from torch.utils.data import Subset
 from torch.cuda import amp
 
 
-#################### CONFIGURAZIONE ####################
+#################### CONFIGURATION ####################
 CONTEXT_PATH = 'resnet18'
 ALPHA = 1
 NUM_CLASSES = 19
@@ -219,7 +219,7 @@ def validate(model, val_loader, criterion, device, num_classes, epoch):
 
 #################### MAIN ####################
 if __name__ == '__main__':
-    print("Avvio training")
+    print("start of training")
     best_miou = 0.0
 
     for epoch in range(1, EPOCHS + 1):
@@ -232,9 +232,9 @@ if __name__ == '__main__':
         if mean_iou > best_miou:
             best_miou = mean_iou
             torch.save(model.state_dict(), 'best_model_3b_blur.pth')
-            print(f"Nuova best accuracy: {best_miou:.2f}% → modello salvato!")
+            print(f"Nuova best accuracy: {best_miou:.2f}% → model saved!")
 
-        print(f"Epoch {epoch} completato! Best accuracy finora: {best_miou:.2f}%\n\n")
+        print(f"Epoch {epoch} completed! Best accuracy so far: {best_miou:.2f}%\n\n")
 
     torch.save(model.state_dict(), f'final_model_epoch_3b_blur{EPOCHS}.pth')
-    print(f"📦 Training finito: modello finale salvato come final_model_epoch_3b_blur{EPOCHS}.pth")
+    print(f"Training finished: final model saved as final_model_epoch_3b_blur{EPOCHS}.pth")
