b828f91dab70bdb35e23371a2de2a045480743a1
Federico Baldi <133745279+fedeCode00@users.noreply.github.com>
2025-06-25 14:59:13 +0200

    Update transforms.py

--- datasets/transforms.py
+++ datasets/transforms.py
@@ -23,7 +23,6 @@ class JointTransform:
         scale = (0.8, 1.0)
         ratio = (1.3, 1.8)
         
-        # Applica con stesso seed
         seed = random.randint(0, 99999)
     
         random.seed(seed)
