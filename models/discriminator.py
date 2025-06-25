c08aceed2447d1062d920e6796f4179d62496921
Federico Baldi <133745279+fedeCode00@users.noreply.github.com>
2025-06-25 16:52:06 +0200

    Update discriminator.py

--- models/discriminator.py
+++ models/discriminator.py
@@ -13,8 +13,6 @@ class FCDiscriminator(nn.Module):
 		self.classifier = nn.Conv2d(ndf*8, 1, kernel_size=4, stride=2, padding=1)
 
 		self.leaky_relu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
-		#self.up_sample = nn.Upsample(scale_factor=32, mode='bilinear')
-		#self.sigmoid = nn.Sigmoid()
 
 
 	def forward(self, x):
@@ -27,7 +25,5 @@ class FCDiscriminator(nn.Module):
 		x = self.conv4(x)
 		x = self.leaky_relu(x)
 		x = self.classifier(x)
-		#x = self.up_sample(x)
-		#x = self.sigmoid(x) 
 
 		return x
